ded129fbdca5491e052d0c6b3c845b9d7f40a986
tholness <23413743+tholness@users.noreply.github.com>
2020-05-17 16:54:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# General
+
+
+# Install
+This package can be installed from the PyPi repository.
+
+`pip install paxful-api`
+
+You can also download the zip and and place into a Virtual Environment created using virtualenv (recommended).
+
+# Setup
+  
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("paxful\\README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="paxful-api", # Replace with your own username
+    version="0.0.1",
+    author="tholness",
+    author_email="support.ec1818@tryninja.io",
+    description="A python wrapper for Paxful REST API v1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tholness/Paxful-Python-API",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.2',
+)
